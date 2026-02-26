0ec5fcbf3214ee94b65a24e4f00d33c427fcf0f9
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-25 22:37:57 -0300

    Add validation na classe ProdutoDTO

--- src/main/java/com/guilhermopayossin/github/ms_produto/controller/ProdutoController.java
+++ src/main/java/com/guilhermopayossin/github/ms_produto/controller/ProdutoController.java
@@ -1,9 +1,7 @@
 package com.guilhermopayossin.github.ms_produto.controller;
 
 import com.guilhermopayossin.github.ms_produto.dto.ProdutoDTO;
-import com.guilhermopayossin.github.ms_produto.enities.Produto;
 import com.guilhermopayossin.github.ms_produto.services.ProdutoService;
-import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
--- src/main/java/com/guilhermopayossin/github/ms_produto/dto/ProdutoDTO.java
+++ src/main/java/com/guilhermopayossin/github/ms_produto/dto/ProdutoDTO.java
@@ -1,6 +1,9 @@
 package com.guilhermopayossin.github.ms_produto.dto;
 
 import com.guilhermopayossin.github.ms_produto.enities.Produto;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,8 +13,15 @@ import lombok.NoArgsConstructor;
 @Getter
 public class ProdutoDTO {
     private Long id;
+
+    @NotBlank(message = "Campo nome é obrigatório")
+    @Size(min = 3, max = 100, message = "O nome deve conter entre 3 e 100 caractéres")
     private String nome;
+    @NotBlank(message = "Campo descrição é obrigatório")
+    @Size(min = 10, message = "a descrição deve conter no mínimo 10 caractéres")
     private String descricao;
+    @NotBlank(message = "Campo valor é obrigatório")
+    @Positive(message = "O campo valor não pode ser menor que 0")
     private Double valor;
 
     public ProdutoDTO(Produto produto) {
