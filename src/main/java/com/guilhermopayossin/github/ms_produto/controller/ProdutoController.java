8abcaf4c069ff08efeb5e9cb5e9e610413602473
Laboratório FIAP <labsfiap@fiap.com.br>
2026-02-25 21:34:33 -0300

    add endpoint getById

--- src/main/java/com/guilhermopayossin/github/ms_produto/controller/ProdutoController.java
+++ src/main/java/com/guilhermopayossin/github/ms_produto/controller/ProdutoController.java
@@ -1,11 +1,34 @@
 package com.guilhermopayossin.github.ms_produto.controller;
 
+import com.guilhermopayossin.github.ms_produto.dto.ProdutoDTO;
+import com.guilhermopayossin.github.ms_produto.services.ProdutoService;
+import org.apache.coyote.Response;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/produtos")
 public class ProdutoController {
 
+    @Autowired
+    private ProdutoService produtoService;
+
+    @GetMapping
+    public ResponseEntity<List<ProdutoDTO>> getAllProdutos() {
+        List<ProdutoDTO> list = produtoService.findAllProdutos();
+        return ResponseEntity.ok(list);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProdutoDTO> getProdutoById(@PathVariable Long id) {
+        ProdutoDTO produtoDTO = produtoService.findProdutoById(id);
+        return ResponseEntity.ok(produtoDTO);
+    }
 
 }
--- src/main/java/com/guilhermopayossin/github/ms_produto/services/ProdutoService.java
+++ src/main/java/com/guilhermopayossin/github/ms_produto/services/ProdutoService.java
@@ -3,6 +3,7 @@ package com.guilhermopayossin.github.ms_produto.services;
 import com.guilhermopayossin.github.ms_produto.dto.ProdutoDTO;
 import com.guilhermopayossin.github.ms_produto.enities.Produto;
 import com.guilhermopayossin.github.ms_produto.repository.ProdutoRepository;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -19,5 +20,13 @@ public class ProdutoService {
     public List<ProdutoDTO> findAllProdutos() {
         List<Produto> produtos = produtoRepository.findAll();
         return produtos.stream().map(ProdutoDTO::new).toList();
+    }
+
+    @Transactional(readOnly = true)
+    public ProdutoDTO findProdutoById(Long id) {
+        Produto produto = produtoRepository.findById(id).orElseThrow(
+                () -> new EntityNotFoundException("Recurso não encontrado: ID - " + id)
+        );
+        return new ProdutoDTO(produto);
     }
 }
